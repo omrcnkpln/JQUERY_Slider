25064a349bf4050de94db3e1554467d3e5679273
pasadagli <kaplan_911@hotmail.com>
2020-12-15 18:05:37 +0300

    changes added

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,14 +1,18 @@
-$yaziItems = $('.content-wrapper .content');
-$imageItems = $('.image-wrapper li');
+var yaziItems = $('.content-wrapper .content');
+var imageItems = $('.image-wrapper li');
 $header = $('header');
-var i = 0;
-r = $yaziItems.length;
+
+var imageWrapper = $("#image-wrapper");
+var itemNumber = imageWrapper.find("li").length;
+var progressAdd = $("#progressBar").width() / (itemNumber - 1);
+var currentProgress = 0;
+var r = yaziItems.length;
+
 $(function () {
     // <!-- _____________________________ main _____________________________________________________________ -->
     var interval;
 
     $("#btnUp").click(function () {
-        // $($yaziItems).removeClass("comeFromBottom comeFromTop goToTop goToBottom");
         addClassesToDown();
         clearInterval(interval);
         timer();
@@ -28,7 +32,6 @@ $(function () {
     });
 
     $("#btnDown").click(function () {
-        // $($yaziItems).removeClass("comeFromBottom comeFromTop goToTop goToBottom");
         addClassesToTop();
         clearInterval(interval);
         timer();
@@ -55,109 +58,145 @@ $(function () {
     };
 
     timer();
-
-
 });
 
 // <!-- _____________________________ yazıların hareketi _____________________________________________________________ -->
+var i = 1;
+var j = 3;
 function addClassesToTop() {
-    progressBar();
-    $($yaziItems).removeClass("comeFromBottom comeFromTop goToTop goToBottom");
+    // bir anda diğer yöne tıklanırsa diye
+    yaziItems.removeClass("comeFromBottom comeFromTop goToTop goToBottom");
 
-    if (i == r - 1) {
-        $yaziItems.eq(0).addClass("show comeFromBottom");
-        $yaziItems.eq(r - 1).removeClass("comeFromBottom").addClass("goToTop");
-        $yaziItems.eq(r - 2).removeClass("show comeFromBottom goToTop");
-    } else {
-        $yaziItems.eq(i + 1).addClass("show comeFromBottom");
-        $yaziItems.eq(i).addClass("goToTop").removeClass("comeFromBottom");
-        $yaziItems.eq(i - 1).removeClass("show comeFromBottom goToTop");
-    }
-    // if (i == 4) {
-    //     $yaziItems.eq(0).addClass("show comeFromBottom");
-    //     $yaziItems.eq(4).removeClass("comeFromBottom").addClass("goToTop");
-    //     $yaziItems.eq(3).removeClass("show comeFromBottom goToTop");
-    // } else {
-    //     $yaziItems.eq(i + 1).addClass("show comeFromBottom");
-    //     $yaziItems.eq(i).addClass("goToTop").removeClass("comeFromBottom");
-    //     $yaziItems.eq(i - 1).removeClass("show comeFromBottom goToTop");
-    // }
+    yaziItems.eq(i - 2).removeClass("show comeFromBottom goToTop");
+    yaziItems.eq(i - 1).addClass("goToTop").removeClass("comeFromBottom");
+    yaziItems.eq(i).addClass("show comeFromBottom");
 
+    // yazılarla ters yönlü
+    imageItems.eq(j - 4).addClass("previous").removeClass("previous-reverse").siblings().removeClass("previous previous-reverse");
+    imageItems.eq(j - 3).addClass("present").removeClass("present-reverse").siblings().removeClass("present present-reverse");
+    imageItems.eq(j - 2).addClass("next").removeClass("next-reverse").siblings().removeClass("next next-reverse");
+    imageItems.eq(j - 1).addClass("down").removeClass("down-reverse").siblings().removeClass("down down-reverse");
 
-    $imageItems.each(function () {
-        if ($(this).hasClass("image-from-top") || $(this).hasClass("image-from-top-reverse")) {
-            $(this).addClass("previous").removeClass("image-from-top image-from-top-reverse");
-        } else if ($(this).hasClass("previous") || $(this).hasClass("previous-reverse")) {
-            $(this).addClass("present").removeClass("previous previous-reverse");
-        } else if ($(this).hasClass("present") || $(this).hasClass("present-reverse")) {
-            $(this).addClass("next").removeClass("present present-reverse");
-        } else if ($(this).hasClass("next") || $(this).hasClass("next-reverse")) {
-            $(this).addClass("image-to-bottom").removeClass("next next-reverse");
-        } else if ($(this).hasClass("image-to-bottom") || $(this).hasClass("image-to-bottom-reverse")) {
-            $(this).addClass("image-from-top").removeClass("image-to-bottom image-to-bottom-reverse");
-        }
-    });
 
     i++;
-    if (i == r) {
+    if (i == yaziItems.length) {
         i = 0;
     }
-    // if (i == 5) {
-    //     i = 0;
-    // }
+
+    j--;
+    if (j == -1) {
+        j = yaziItems.length-1;
+    }
+
+    progressBarRight();
 }
 
 function addClassesToDown() {
-    progressBar();
-    $($yaziItems).removeClass("comeFromBottom comeFromTop goToTop goToBottom");
+    // bir anda diğer yöne tıklanırsa diye
+    yaziItems.removeClass("comeFromBottom comeFromTop goToTop goToBottom");
 
-    if (i == 0) {
-        $yaziItems.eq(r - 1).addClass("show comeFromTop");
-        $yaziItems.eq(0).removeClass("comeFromTop").addClass("goToBottom");
-        $yaziItems.eq(1).removeClass("show comeFromTop goToBottom");
-    } else {
-        $yaziItems.eq(0).removeClass("show");
-        $yaziItems.eq(i - 1).addClass("show comeFromTop");
-        $yaziItems.eq(i).addClass("goToBottom").removeClass("comeFromTop");
-        $yaziItems.eq(i + 1).removeClass("show comeFromTop goToBottom");
-    }
-    // if (i == 0) {
-    //     $yaziItems.eq(4).addClass("show comeFromTop");
-    //     $yaziItems.eq(0).removeClass("comeFromTop").addClass("goToBottom");
-    //     $yaziItems.eq(1).removeClass("show comeFromTop goToBottom");
-    // } else {
-    //     $yaziItems.eq(0).removeClass("show");
-    //     $yaziItems.eq(i - 1).addClass("show comeFromTop");
-    //     $yaziItems.eq(i).addClass("goToBottom").removeClass("comeFromTop");
-    //     $yaziItems.eq(i + 1).removeClass("show comeFromTop goToBottom");
-    // }
+    yaziItems.eq(i - 2).addClass("show comeFromTop");
+    yaziItems.eq(i - 1).addClass("goToBottom").removeClass("comeFromTop");
+    yaziItems.eq(i).removeClass("show comeFromTop goToBottom");
 
-    // <!-- _____________________________ for images _____________________________________________________________ -->
-    $imageItems.each(function () {
-        if ($(this).hasClass("image-from-top") || $(this).hasClass("image-from-top-reverse")) {
-            $(this).addClass("image-to-bottom-reverse").removeClass("image-from-top image-from-top-reverse");
-        }
-        else if ($(this).hasClass("image-to-bottom") || $(this).hasClass("image-to-bottom-reverse")) {
-            $(this).addClass("next-reverse").removeClass("image-to-bottom image-to-bottom-reverse");
-        }
-        else if ($(this).hasClass("next") || $(this).hasClass("next-reverse")) {
-            $(this).addClass("present-reverse").removeClass("next next-reverse");
-        }
-        else if ($(this).hasClass("present") || $(this).hasClass("present-reverse")) {
-            $(this).addClass("previous-reverse").removeClass("present present-reverse");
-        }
-        else if ($(this).hasClass("previous") || $(this).hasClass("previous-reverse")) {
-            $(this).addClass("image-from-top-reverse").removeClass("previous previous-reverse");
-        }
-    })
+    // yazılarla aynı yönlü
+    // imageItems.eq(i - 4).addClass("next").siblings().removeClass("next next-reverse");
+    // imageItems.eq(i - 3).addClass("image-to-bottom").siblings().removeClass("image-to-bottom image-to-bottom-reverse");
+    // imageItems.eq(i - 2).addClass("image-from-top").siblings().removeClass("image-from-top image-from-top-reverse");
+    // imageItems.eq(i - 1).addClass("previous").siblings().removeClass("previous previous-reverse");
+    // imageItems.eq(i).addClass("present").siblings().removeClass("present present-reverse");
+
+    // yazılarla ters yönlü 
+    imageItems.eq(j - 3).addClass("previous-reverse").removeClass("previous").siblings().removeClass("previous previous-reverse");
+    imageItems.eq(j - 2).addClass("present-reverse").removeClass("present").siblings().removeClass("present present-reverse");
+    imageItems.eq(j - 1).addClass("next-reverse").removeClass("next").siblings().removeClass("next next-reverse");
+    imageItems.eq(j).addClass("down-reverse").removeClass("down").siblings().removeClass("down down-reverse");
+
+
     i--;
     if (i == -1) {
-        i = r - 1;
+        i = yaziItems.length - 1;
     }
-    // if (i == -1) {
-    //     i = 4;
-    // }
+
+    j++;
+    if (j == yaziItems.length) {
+        j = 0;
+    }
+
+    progressBarLeft();
 }
+
+function progressBarRight() {
+    var w = $(window).width();
+
+
+    progressAdd = $("#progressBar").width() / (itemNumber - 1);
+    currentProgress = currentProgress + progressAdd;
+
+
+    if (currentProgress - 10 > $("#progressBar").width()) {
+        currentProgress = 0;
+    }
+
+    $("#progressBar .progress-bar").animate({ width: currentProgress });
+
+    $(".count").removeClass("animate__fadeInRight animate__fadeInUp");
+    if (w >= 576) {
+        setTimeout(() => {
+            $(".count").addClass("animate__fadeInRight");
+            if (i == 0) {
+                $("#count").text('0' + (itemNumber));
+            } else {
+                $("#count").text('0' + (i));
+            }
+        }, 1000);
+    } else {
+        setTimeout(() => {
+            $(".count").addClass("animate__fadeInUp");
+            if (i == 0) {
+                $("#count").text('0' + (itemNumber));
+            } else {
+                $("#count").text('0' + (i));
+            }
+        }, 1000);
+    }
+};
+
+function progressBarLeft() {
+    var w = $(window).width();
+
+    progressAdd = $("#progressBar").width() / (itemNumber - 1);
+    currentProgress = currentProgress - progressAdd;
+
+
+    if (currentProgress < -10) {
+        currentProgress = $("#progressBar").width();
+    }
+
+    $("#progressBar .progress-bar").animate({ width: currentProgress });
+
+    $(".count").removeClass("animate__fadeInRight animate__fadeInUp");
+
+    if (w >= 576) {
+        setTimeout(() => {
+            $(".count").addClass("animate__fadeInRight");
+            if (i == 0) {
+                $("#count").text('0' + (itemNumber));
+            } else {
+                $("#count").text('0' + (i));
+            }
+        }, 1000);
+    } else {
+        setTimeout(() => {
+            $(".count").addClass("animate__fadeInUp");
+            if (i == 0) {
+                $("#count").text('0' + (itemNumber));
+            } else {
+                $("#count").text('0' + (i));
+            }
+        }, 1000);
+    }
+};
 
 function changeHeaderBg() {
     if (i == 0) {
@@ -172,123 +211,3 @@ function changeHeaderBg() {
         $header.removeClass().addClass("bg-3");
     }
 }
-
-function progressBar() {
-    var w = $(window).width();
-    if (i == 0) {
-        $("#progressBar").addClass("progress-2");
-
-        $(".count").removeClass("animate__fadeInRight animate__fadeInUp");
-
-        if(w >= 576){
-            setTimeout(() => {
-                $(".count").addClass("animate__fadeInRight");
-                $("#count").text("02");
-            }, 1000);
-        }else{
-            setTimeout(() => {
-                $(".count").addClass("animate__fadeInUp");
-                $("#count").text("02");
-            }, 1000);
-        }
-    }
-    else if (i == 1) {
-        $("#progressBar").addClass("progress-3").removeClass("progress-2");
-
-        $(".count").removeClass("animate__fadeInRight animate__fadeInUp");
-
-        if(w >= 576){
-            setTimeout(() => {
-                $(".count").addClass("animate__fadeInRight");
-                $("#count").text("03");
-            }, 1000);
-        }else{
-            setTimeout(() => {
-                $(".count").addClass("animate__fadeInUp");
-                $("#count").text("03");
-            }, 1000);
-        }
-    } else if (i == 2) {
-        $("#progressBar").addClass("progress-4").removeClass("progress-3");
-
-        $(".count").removeClass("animate__fadeInRight animate__fadeInUp");
-
-        if(w >= 576){
-            setTimeout(() => {
-                $(".count").addClass("animate__fadeInRight");
-                $("#count").text("04");
-            }, 1000);
-        }else{
-            setTimeout(() => {
-                $(".count").addClass("animate__fadeInUp");
-                $("#count").text("04");
-            }, 1000);
-        }
-    } else if (i == 3) {
-        $("#progressBar").addClass("progress-5").removeClass("progress-4");
-
-        $(".count").removeClass("animate__fadeInRight animate__fadeInUp");
-
-        if(w >= 576){
-            setTimeout(() => {
-                $(".count").addClass("animate__fadeInRight");
-                $("#count").text("05");
-            }, 1000);
-        }else{
-            setTimeout(() => {
-                $(".count").addClass("animate__fadeInUp");
-                $("#count").text("05");
-            }, 1000);
-        }
-    }else if (i == 4) {
-        $("#progressBar").removeClass().addClass("progress-bar animate__animated progress-1");
-
-        $(".count").removeClass("animate__fadeInRight animate__fadeInUp");
-
-        if(w >= 576){
-            setTimeout(() => {
-                $(".count").addClass("animate__fadeInRight");
-                $("#count").text("01");
-            }, 1000);
-        }else{
-            setTimeout(() => {
-                $(".count").addClass("animate__fadeInUp");
-                $("#count").text("01");
-            }, 1000);
-        }
-    }
-    //  else if (i == 4) {
-    //     $("#progressBar").addClass("progress-6").removeClass("progress-5");
-
-    //     $(".count").removeClass("animate__fadeInRight animate__fadeInUp");
-
-    //     if(w >= 576){
-    //         setTimeout(() => {
-    //             $(".count").addClass("animate__fadeInRight");
-    //             $("#count").text("06");
-    //         }, 1000);
-    //     }else{
-    //         setTimeout(() => {
-    //             $(".count").addClass("animate__fadeInUp");
-    //             $("#count").text("06");
-    //         }, 1000);
-    //     }
-    // } 
-    // else if (i == 5) {
-    //     $("#progressBar").removeClass().addClass("progress-bar animate__animated progress-1");
-
-    //     $(".count").removeClass("animate__fadeInRight animate__fadeInUp");
-
-    //     if(w >= 576){
-    //         setTimeout(() => {
-    //             $(".count").addClass("animate__fadeInRight");
-    //             $("#count").text("01");
-    //         }, 1000);
-    //     }else{
-    //         setTimeout(() => {
-    //             $(".count").addClass("animate__fadeInUp");
-    //             $("#count").text("01");
-    //         }, 1000);
-    //     }
-    // }
-};
